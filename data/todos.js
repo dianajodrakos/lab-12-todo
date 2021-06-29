61d3d54ef2c1059657d788a7d3fb78802730360e
DJ Drakos <dianajodrakos@gmail.com>
2021-06-29 13:51:33 -0700

    changed /data scripts

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'pay rent',
+    completed: false,
+  },
+  {
+    todo: 'water plants',
+    completed: false,
+  },
+  {
+    todo: 'text bae',
+    completed: false,
+  }
+];
