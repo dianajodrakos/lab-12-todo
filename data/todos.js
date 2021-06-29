587b2a1ad323ec87392a5fd0eeb67f10856a3a92
DJ Drakos <dianajodrakos@gmail.com>
2021-06-29 16:23:45 -0700

    added GET endpoint

--- data/todos.js
+++ data/todos.js
@@ -2,16 +2,16 @@ module.exports = [
   {
     todo: 'pay rent',
     completed: false,
-    user_id: 1,
+    user_id: 2,
   },
   {
     todo: 'water plants',
     completed: false,
-    user_id: 1,
+    user_id: 2,
   },
   {
     todo: 'text bae',
     completed: false,
-    user_id: 1,
+    user_id: 2,
   }
 ];
